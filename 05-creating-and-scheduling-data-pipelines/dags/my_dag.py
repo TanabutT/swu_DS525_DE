8dfbdbdbac263b444561f80d0e0427f237e8b44b
TanabutT <t_tanabut@hotmail.com>
2022-10-30 00:07:50 +0700

    dag1

--- 05-creating-and-scheduling-data-pipelines/dags/my_dag.py
+++ 05-creating-and-scheduling-data-pipelines/dags/my_dag.py
@@ -1,10 +1,31 @@
 from airflow import DAG
 from airflow.utils import timezone
 from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
 with DAG (
     "my_dag",
     start_date=timezone.datetime(2022, 10, 8),
-    schedule=None,
+    schedule=None, # use cron " * * * * *"
+    tags=["workshop"],
 ):
     t1 = EmptyOperator(task_id="t1")
+
+    echo_hello = BashOperator(
+        task_id="echo_hello",
+        bash_command="echo 'hello'",
+
+    )
+
+    def _print_hey():
+        print("Hey!")
+
+    print_hey = PythonOperator(
+        task_id="print_hey",
+        python_callable=_print_hey,
+    )
+
     t2 = EmptyOperator(task_id="t2")
+
+    t1 >> echo_hello >> print_hey >> t2
