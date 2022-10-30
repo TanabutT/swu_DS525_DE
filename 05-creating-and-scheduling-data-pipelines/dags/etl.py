3c62088e365bd00e60fe275901080b9c002e428c
TanabutT <t_tanabut@hotmail.com>
2022-10-30 22:55:08 +0700

    data copy and etl dag

---
+++ 05-creating-and-scheduling-data-pipelines/dags/etl.py
@@ -0,0 +1,39 @@
+from airflow import DAG
+from airflow.utils import timezone
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+
+def _get_files(filepath: str) -> List[str]:
+    """
+    Description: This function is responsible for listing the files in a directory
+    """
+
+    all_files = []
+    for root, dirs, files in os.walk(filepath):
+        files = glob.glob(os.path.join(root, "*.json"))
+        for f in files:
+            all_files.append(os.path.abspath(f))
+
+    num_files = len(all_files)
+    print(f"{num_files} files found in {filepath}")
+
+    return all_files
+
+
+with DAG (
+    "etl",
+    start_date=timezone.datetime(2022, 10, 8),
+    schedule="@daily", # use cron " * * * * *"
+    tags=["workshop"],
+    catchup=False,
+) as dag:
+
+    get_files = PythonOperator(
+        task_id="get_file",
+        python_callable=_get_files,
+        op_kwargs={
+            "filepath" : "./data",
+        }
+    )
