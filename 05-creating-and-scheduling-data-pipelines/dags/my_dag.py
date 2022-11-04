030056e9e003c495d766c32e243ad8a1aff6d25b
TanabutT <t_tanabut@hotmail.com>
2022-11-04 12:56:13 +0700

    update my_dag.py

--- 05-creating-and-scheduling-data-pipelines/dags/my_dag.py
+++ 05-creating-and-scheduling-data-pipelines/dags/my_dag.py
@@ -1,32 +1,51 @@
+import logging
+
 from airflow import DAG
 from airflow.utils import timezone
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
-with DAG (
+
+def _say_hello(name="", **context):
+    print(context)
+    datestamp = context["ds"]
+    ds_nodash = context["ds_nodash"]
+    print(f"Hello! {name} on {datestamp}")
+
+
+def _print_log_messages():
+    logging.info("Hello from Log")
+
+
+with DAG(
     "my_dag",
-    start_date=timezone.datetime(2022, 10, 8),
-    schedule=None, # use cron " * * * * *"
+    start_date=timezone.datetime(2022, 10, 15),
+    schedule="*/30 * * * *",
     tags=["workshop"],
-):
-    t1 = EmptyOperator(task_id="t1")
+    catchup=False,
+) as dag:
+
+    start = EmptyOperator(task_id="start")
 
     echo_hello = BashOperator(
         task_id="echo_hello",
-        bash_command="echo 'hello'",
-
+        bash_command="echo 'hello' on {{ ds }}",
     )
 
-    def _print_hey():
-        print("Hey!")
-
-    print_hey = PythonOperator(
-        task_id="print_hey",
-        python_callable=_print_hey,
+    say_hello = PythonOperator(
+        task_id="say_hello",
+        python_callable=_say_hello,
+        op_kwargs={
+            "name": "Kan {{ ds_nodash }}",
+        }
     )
 
-    t2 = EmptyOperator(task_id="t2")
+    print_log_messages = PythonOperator(
+        task_id="print_log_messages",
+        python_callable=_print_log_messages,
+    )
 
-    # t1 >> echo_hello >> print_hey >> t2
-    t1 >> [echo_hello , print_hey] >> t2
+    end = EmptyOperator(task_id="end")
+
+    start >> echo_hello >> [say_hello, print_log_messages] >> end
